e6a438bcd4310d4407a51f3bb10bc07e6108009f
Jelena <47182797+JDugonjic@users.noreply.github.com>
2021-02-03 17:05:42 +0100

    Post upload added

--- src/App.js
+++ src/App.js
@@ -57,14 +57,16 @@ function App() {
   }, [user, username]);
 
   useEffect(() => {
-    db.collection("posts").onSnapshot((snapshot) => {
-      setPosts(
-        snapshot.docs.map((doc) => ({
-          id: doc.id,
-          post: doc.data(),
-        }))
-      );
-    });
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            post: doc.data(),
+          }))
+        );
+      });
   }, []);
 
   const signUp = (e) => {
@@ -90,8 +92,6 @@ function App() {
 
   return (
     <div className="app">
-      <ImageUpload />
-
       <Modal open={open} onClose={() => setOpen(false)}>
         <div style={modalStyle} className={classes.paper}>
           <form className="app__signUp">
@@ -162,17 +162,17 @@ function App() {
           src="https://logos-world.net/wp-content/uploads/2020/04/Instagram-Logo.png"
           alt=""
         />
+        {user ? (
+          <Button onClick={() => auth.signOut()}>Logout</Button>
+        ) : (
+          <div className="app__loginContainer">
+            <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+
+            <Button onClick={() => setOpen(true)}>Sign Up</Button>
+          </div>
+        )}
       </div>
 
-      {user ? (
-        <Button onClick={() => auth.signOut()}>Logout</Button>
-      ) : (
-        <div className="app__loginContainer">
-          <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
-
-          <Button onClick={() => setOpen(true)}>Sign Up</Button>
-        </div>
-      )}
       {posts.map(({ id, post }) => (
         <Post
           key={id}
@@ -181,6 +181,11 @@ function App() {
           imageUrl={post.imageUrl}
         />
       ))}
+      {user?.displayName ? (
+        <ImageUpload username={user.displayName} />
+      ) : (
+        <h3>Please login to upload</h3>
+      )}
     </div>
   );
 }
--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -1,9 +1,10 @@
 import { Button } from "@material-ui/core";
 import React, { useState } from "react";
-import { storage } from "./firebase";
+import { db, storage } from "./firebase";
 import "./ImageUpload.css";
+import firebase from "firebase";
 
-function ImageUpload() {
+function ImageUpload({ username }) {
   const [caption, setCaption] = useState("");
   const [progress, setProgress] = useState(0);
   const [image, setImage] = useState(null);
@@ -16,13 +17,44 @@ function ImageUpload() {
 
   const handleUpload = () => {
     const uploadTask = storage.ref(`images/${image.name}`).put(image);
-    uploadTask.on("state_changed", (snapshot) => {
-      // progress function ...
-    });
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // progress function ...
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgress(progress);
+      },
+      (error) => {
+        // Error function ...
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        // Complete function
+        storage
+          .ref("images")
+          .child(image.name)
+          .getDownloadURL()
+          .then((url) => {
+            db.collection("posts").add({
+              timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+              caption: caption,
+              imageUrl: url,
+              username: username,
+            });
+            setProgress(0);
+            setCaption("");
+            setImage(null);
+          });
+      }
+    );
   };
 
   return (
-    <div>
+    <div className="imageUpload">
+      <progress className="imageUpload__progress" value={progress} max="100" />
       <input
         type="text"
         placeholder="Enter a caption"
